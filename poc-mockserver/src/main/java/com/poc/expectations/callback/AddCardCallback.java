84d9fbf647018d2c88be343a31fc40b526c6d4d4
Welson Carvalho <welson@gocariq.com>
2022-01-21 23:36:41 +0100

    added some dynamic fields for addcardresponse

--- poc-mockserver/src/main/java/com/poc/expectations/callback/AddCardCallback.java
+++ poc-mockserver/src/main/java/com/poc/expectations/callback/AddCardCallback.java
@@ -3,21 +3,28 @@ package com.poc.expectations.callback;
 import com.poc.expectations.utils.RandomValuesUtil;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 import org.mockserver.mock.action.ExpectationResponseCallback;
 import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 
 public class AddCardCallback implements ExpectationResponseCallback {
 
+    private static final String SPACE = " ";
+
     @Override
     public HttpResponse handle(HttpRequest httpRequest) throws Exception {
+        JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+        JSONObject request = (JSONObject) parser.parse(httpRequest.getBodyAsJsonOrXmlString());
+        JSONObject addCard = (JSONObject) request.get("addCard");
+
         return HttpResponse.response()
                 .withStatusCode(200)
                 .withHeader("Content-Type", "application/json")
-                .withBody(response().toJSONString());
+                .withBody(response(addCard).toJSONString());
     }
 
-    private JSONObject response() {
+    private JSONObject response(JSONObject addCard) {
         JSONObject response = new JSONObject();
 
         response.put("responseCode", "00");
@@ -25,14 +32,14 @@ public class AddCardCallback implements ExpectationResponseCallback {
         response.put("newCardNumber", newCardNumber());
         response.put("sharingCards", sharingCards());
         response.put("balance", 0.0);
-        response.put("transId", "mock-transId");
+        response.put("transId", transId());
         response.put("arn", "mock-arn");
         response.put("clerkId", "mock-clerkId");
         response.put("customerId", "110000000009999999");
         response.put("fee", 0.0);
         response.put("referenceId", RandomValuesUtil.stringNumbers(15));
-        response.put("batchReferenceId", "mock-batchReferenceId");
-        response.put("nameOnCard", "Mock Name");
+        response.put("batchReferenceId", RandomValuesUtil.stringNumbers(20));
+        response.put("nameOnCard", nameOnCard(addCard));
 
         return response;
     }
@@ -56,5 +63,34 @@ public class AddCardCallback implements ExpectationResponseCallback {
         sharingCard.put("sharingCardNumber", number);
         sharingCard.put("sharingCardReferenceId", referenceId);
         return sharingCard;
+    }
+
+    private String transId() {
+        return "D2" + RandomValuesUtil.stringNumbers(9);
+    }
+
+    private String nameOnCard(JSONObject addCard) {
+        JSONObject profile = (JSONObject) addCard.get("profile");
+
+        if (profile != null) {
+
+            StringBuilder nameOnCard = new StringBuilder();
+
+            nameOnCard.append(profile.getAsString("firstName"));
+
+            if (profile.containsKey("middleName")) {
+                nameOnCard.append(SPACE);
+                nameOnCard.append(profile.getAsString("middleName"));
+            }
+
+            if (profile.containsKey("lastName")) {
+                nameOnCard.append(SPACE);
+                nameOnCard.append(profile.getAsString("lastName"));
+            }
+
+            return (nameOnCard.length() < 60) ? nameOnCard.toString() : nameOnCard.substring(0, 59);
+        }
+
+        return null;
     }
 }
